4b0b107986fe53f7982c1ed944b6971a21f6a4f1
AntoninoIemmolo <antoninoiemmolo6@gmail.com>
2023-11-03 20:36:12 +0100

    Copiato il main dell'anno scorso come main del Lab1 ES 2

--- Lab_1/ES2/main.c
+++ Lab_1/ES2/main.c
@@ -1,4 +1,4 @@
-
+//
 //  main.c
 //  LAB6_Esercizio1
 //
@@ -7,31 +7,30 @@
 
 #include <stdio.h>
 #include <string.h>
-#define MaxDati 10
+
 typedef struct {
-    char Cod_trat[7];
-    char Part[15];
-    char Dest[15];
-    char Data[11];
-    char Ora_Part[11];
-    char Ora_Arri[11];
+    char Cod_trat[30];
+    char Part[30];
+    char Dest[30];
+    char Data[30];
+    char Ora_Part[30];
+    char Ora_Arri[30];
     int Rit;
 }T_TRATTA;
 int Cod_Operazione(char Oper[10]);
-void Svolgi_Operazione(T_TRATTA V_Trat[MaxDati], int Num_Tratte, int scelta);
+void Svolgi_Operazione(T_TRATTA V_Trat[1000], int Num_Tratte, int scelta);
 int Conv_data(int anno, int mese, int gioro);
-void Ordina (T_TRATTA* V_Trat, T_TRATTA** Vet_ord,int campo);
-void Converti(T_TRATTA* Corsa1,T_TRATTA* Corsa2,int campo,unsigned long long Vet_tmp[2]);
-int  Cod_Operazione_Ordinamento(char Oper[]);
+
 
 
 int main(int argc, const char * argv[]) {
-    T_TRATTA Vet_Tratte[MaxDati]={0}, t,*Vet_ord[MaxDati]; 
-    int N_Tratte=0, Codice_Operazione = 0,Codice_Operazione2=-1;
-    FILE* Fin,*Fout;
-    char operazione[20],tmp[10];
-	Fout = fopen("stampa.txt","w");
+    T_TRATTA Vet_Tratte[1000], t;
+    
+    int N_Tratte=0, Codice_Operazione = 0;
+    FILE* Fin;
+    char operazione[20];
     Fin = fopen("log.txt", "r");
+    if(Fin==NULL) return -1;
     fscanf(Fin, "%d",&N_Tratte);
 
     for(int i=0;i<N_Tratte;i++){
@@ -39,36 +38,11 @@ int main(int argc, const char * argv[]) {
         Vet_Tratte[i] = t;
     }
     while (Codice_Operazione != 6) {
-        printf("inserire l'operazione che si vuole svolgere: ");
+        printf("inserire l'operazione che si vuole svolgere:");
         scanf("%s", operazione);
         Codice_Operazione = Cod_Operazione(operazione);
         Svolgi_Operazione(Vet_Tratte,N_Tratte,Codice_Operazione);
     }
-	for (int i=0;i<MaxDati;i++){
-		Vet_ord[i]=&Vet_Tratte[i];
-	}
-    while (Codice_Operazione2 != 0) {
-        printf("inserire l'oridinamento che si vuole svolgere: ");
-        scanf("%s", operazione);
-        Codice_Operazione2 = Cod_Operazione_Ordinamento(operazione);
-	if(Codice_Operazione2==5){
-		printf("si vuole stampare a video o su file?\n");
-		scanf("%s",tmp);
-		for (int i=0;i<MaxDati;i++){
-			if(strcmp(tmp,"video")==0)
-			printf("[%d] = (Cod_trat = %s, Part = %s, Dest = %s, Data = %s, Ora Part = %s, Ora Arrivo = %s, Rit = %d)\n",i,Vet_Tratte[i].Cod_trat,Vet_Tratte[i].Part,Vet_Tratte[i].Dest,Vet_Tratte[i].Data,Vet_Tratte[i].Ora_Part,Vet_Tratte[i].Ora_Arri,Vet_Tratte[i].Rit);
-			else if(strcmp(tmp,"file")==0){
-				fprintf(Fout,"[%d] = (Cod_trat = %s, Part = %s, Dest = %s, Data = %s, Ora Part = %s, Ora Arrivo = %s, Rit = %d)\n",i,Vet_Tratte[i].Cod_trat,Vet_Tratte[i].Part,Vet_Tratte[i].Dest,Vet_Tratte[i].Data,Vet_Tratte[i].Ora_Part,Vet_Tratte[i].Ora_Arri,Vet_Tratte[i].Rit);
-
-			}
-		}
-
-	}
-	else Ordina (Vet_Tratte,Vet_ord,Codice_Operazione2);
-
-    }
-
-	fclose(Fout); fclose(Fin);
     return 0;
 }
 
@@ -79,22 +53,13 @@ int  Cod_Operazione(char Oper[]){
     else if(strcmp(Oper, "ritardo")==0) return 4;
     else if(strcmp(Oper, "ritardo_tot")==0) return 5;
     else if(strcmp(Oper, "fine")==0) return 6;
-    else{ printf("errore operazione no nriconosciuta\n"); return 0;}
+    else{ printf("errore operazione non riconosciuta\n"); return 0;}
 }
-int  Cod_Operazione_Ordinamento(char Oper[]){
-    if(strcmp(Oper, "data")==0) return 1;
-    else if(strcmp(Oper, "tratta")==0) return 2;
-    else if(strcmp(Oper, "partenza")==0) return 3;
-    else if(strcmp(Oper, "arrivo")==0) return 4;
-    else if(strcmp(Oper, "stampa")==0) return 5;
-    else if(strcmp(Oper, "ricerca")==0) return 6;
-    else if(strcmp(Oper, "fine")==0) return 0;
-    else{ printf("errore operazione non riconosciuta\n"); return -1;}
-}
-int Conv_data(int anno,int mese,int giorno){
+
+int Conv_data(int anno, int mese, int giorno){
     int Data=0;
     Data = anno*10000+mese*100+giorno;
-	    return Data;
+    return Data;
 }
 
 
@@ -107,10 +72,10 @@ void Svolgi_Operazione(T_TRATTA V_Trat[], int Num_Tratte, int scelta){
         case 1:
             printf("\ninserire le date nel formato giorno/mese/anno: ");
             printf("\n1) ");
-            scanf("%d/%d/%d",&anno,&mese,&giorno);
+            scanf("%d/%d/%d",&giorno,&mese,&anno);
             DataI = Conv_data(anno, mese, giorno);
             printf("\n2) ");
-            scanf("%d/%d/%d",&anno,&mese,&giorno);
+	    scanf("%d/%d/%d",&giorno,&mese,&anno);
             DataF = Conv_data(anno, mese, giorno);
             for (int i=0;i<Num_Tratte;i++){
                 sscanf(V_Trat[i].Data, "%d/%d/%d",&tmp[0],&tmp[1],&tmp[2]);
@@ -125,20 +90,18 @@ void Svolgi_Operazione(T_TRATTA V_Trat[], int Num_Tratte, int scelta){
             printf("inserire la partenza di interesse:\n");
             scanf("%s", Partenza);
             for (int i=0;i<Num_Tratte;i++){
-                if(strcmp(V_Trat[i].Part,Partenza)==0) 
-			printf("%s è partito da %s\n",V_Trat[i].Cod_trat, Partenza);
+                if(strcmp(V_Trat[i].Part,Partenza)==0) printf("%s è partito da %s\n",V_Trat[i].Cod_trat, Partenza);
             }
             break;
         case 3:
             printf("inserire la destinazione di interesse:\n");
             scanf("%s", Destinazione);
             for (int i=0;i<Num_Tratte;i++){
-                if(strcmp(V_Trat[i].Dest,Destinazione)==0)
-			printf("%s è partito da %s\n",V_Trat[i].Cod_trat, Destinazione);
+                if(strcmp(V_Trat[i].Dest,Destinazione)==0) printf("%s è partito da %s\n",V_Trat[i].Cod_trat, Destinazione);
             }
             break;
         case 4:
-            printf("\ninserire le date nel formato anno/mese/giorno: ");
+            printf("\ninserire le date nel formato giorno/mese/anno: ");
             printf("\n1) ");
             scanf("%d/%d/%d",&anno,&mese,&giorno);
             DataI = Conv_data(anno, mese, giorno);
@@ -170,76 +133,5 @@ void Svolgi_Operazione(T_TRATTA V_Trat[], int Num_Tratte, int scelta){
                 printf("Grazie e arrivedereci\n");
                 break;
 
-	}
-}
-void Ordina (T_TRATTA* V_Trat,T_TRATTA** Vet_ord,int campo){
-	T_TRATTA *tmp;
-	unsigned long long Vet_tmp[2];
-	for(int i=0;i<MaxDati;i++){
-		for (int j=1;j<MaxDati-i;j++){
-			Converti(Vet_ord[j-1],Vet_ord[j],campo,Vet_tmp);
-			if (Vet_tmp[0]>Vet_tmp[1]){
-				tmp = Vet_ord[j];
-				Vet_ord[j]=Vet_ord[j-1];
-				Vet_ord[j-1]=tmp;
-			}
-		}
-	}
-}
-//ritorna l'indece del primo carattere che differisce tra str1 e str2
-int ComparaStringhe(char* string1,char* string2){
-	int i=0;
-	char str1[15],str2[15];
-	strcpy(str1,string1);
-	strcpy(str2,string2);
-	if((int)(str1[i])<95&&str1[i]!='\0') str1[i]=str1[i]+32;
-	if((int)(str2[i])<95&&str2[i]!='\0') str2[i]=str2[i]+32;
-	while(str1[i]==str2[i]&&(str1[i]!='\0'&&str2[i]!='\0')){
-		if((int)(str1[i+1])<95&&str1[i+1]!='\0') str1[i+1]=str1[i+1]+32;
-		if((int)(str2[i+1])<95&&str2[i+1]!='\0') str2[i+1]=str2[i+1]+32;
-		i++;
-	}
-	return i;
-}
-void Converti(T_TRATTA* corsa1,T_TRATTA* corsa2,int campo,unsigned long long Vet_tmp[2]){
-	int Data_vet[3],indice;
-	switch(campo){
-		//Data+ora
-		case(1):
-			sscanf(corsa1->Data, "%d/%d/%d",&Data_vet[0],&Data_vet[1],&Data_vet[2]);
-			Vet_tmp[0]=Conv_data(Data_vet[0],Data_vet[1],Data_vet[2]);
-			sscanf(corsa1->Ora_Part, "%d:%d:%d",&Data_vet[0],&Data_vet[1],&Data_vet[2]);
-			Vet_tmp[0]=Vet_tmp[0]*1000000+Conv_data(Data_vet[0],Data_vet[1],Data_vet[2]);
-			sscanf(corsa2->Data, "%d/%d/%d",&Data_vet[0],&Data_vet[1],&Data_vet[2]);
-			Vet_tmp[1]=Conv_data(Data_vet[0],Data_vet[1],Data_vet[2]);
-			sscanf(corsa2->Ora_Part, "%d:%d:%d",&Data_vet[0],&Data_vet[1],&Data_vet[2]);
-			Vet_tmp[1]=Vet_tmp[1]*1000000+Conv_data(Data_vet[0],Data_vet[1],Data_vet[2]);
-			break;
-		
-		//Cod Tratta
-		case(2):
-			sscanf(corsa1->Cod_trat,"GTT%lld",&Vet_tmp[0]);
-			sscanf(corsa2->Cod_trat,"GTT%lld",&Vet_tmp[1]);
-			if(strcmp(corsa1->Cod_trat,"")==0) Vet_tmp[0]=0;
-			if(strcmp(corsa2->Cod_trat,"")==0) Vet_tmp[1]=0;
-			break;
-		
-		//ordine alfabetico Partenza
-		case(3):
-			indice = ComparaStringhe(corsa1->Part,corsa2->Part);
-			Vet_tmp[0]=(int)(corsa1->Part[indice]);
-			Vet_tmp[1]=(int)(corsa2->Part[indice]);
-			break;
-		
-		case(4):
-			indice = ComparaStringhe(corsa1->Dest,corsa2->Dest);
-			Vet_tmp[0]=(int)(corsa1->Dest[indice]);
-			Vet_tmp[1]=(int)(corsa2->Dest[indice]);
-			break;
-		
-		case(0):
-			break;
-		
-
-	}
-}
+        }
+    }
