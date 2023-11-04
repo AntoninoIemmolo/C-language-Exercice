09cb13f132e9ced7dbe557387e44395b92d360e3
AntoninoIemmolo <antoninoiemmolo6@gmail.com>
2023-11-04 14:49:08 +0100

    Ultimato LAB3 ES1

--- Lab_3/ES1/main.c
+++ Lab_3/ES1/main.c
@@ -9,6 +9,7 @@ int majority( int *Vet, int N){
 			Max=Vet[i];
 		}
 	}
+	//Dynamimc allocation of Point array, free at line:
 	Point=(int *)malloc(Max*sizeof(int));
 	for (int i=0;i<Max;i++)
 		Point[i]=0;
@@ -19,20 +20,23 @@ int majority( int *Vet, int N){
 
 }
 int majority_ric(int* Vet,int N,int *Point,int Targ,int ReqOccur){
-	int i=0,RetVal=-1;
-	while(i<N && Vet[i]==Vet[i+1]) 
+	//i variable rapresents the numbers of reading in Vet that this call has made
+	//the minimum ammount of readinf for eeach call is set to 1
+	int i=1,RetVal=-1;
+	while(i<N && Vet[i-1]==Vet[i]) 
 		i++;
-	Point[Vet[i]] = Point[Vet[i]] + i + 1;
-	if(Point[Vet[i]]>ReqOccur) 
-		return Vet[i];
-	if(i<N-1)
-		RetVal=majority_ric(&Vet[i+1],N-i-1,Point,Vet[i+1],ReqOccur);
+	Point[Vet[i-1]] = Point[Vet[i-1]] + i;
+	if(Point[Vet[i-1]]>ReqOccur) 
+		return Vet[i-1];
+	//if this recursive call hasn't reach the max
+	if(i<N)
+		RetVal=majority_ric(&Vet[i],N-i,Point,Vet[i],ReqOccur);
 	return RetVal;
 }
 
 //main di prova
 int main(int argn,char* argv[]){
-	int Vet[MaxDati]={3, 3, 9, 4, 3, 5, 4},Resoult,N=MaxDati;
+	int Vet[MaxDati]={2, 3, 2, 2, 3, 2, 3},Resoult,N=MaxDati;
 	Resoult=majority(Vet,N);
 	printf("%d\n",Resoult);
 	return 0;
