e39776cdcd4203c41f11887862f8bbc7f76b0671
AntoninoIemmolo <antoninoiemmolo6@gmail.com>
2023-11-25 19:04:23 +0100

    aggiornato L4 E3

--- L04/E03/main.c
+++ L04/E03/main.c
@@ -1,149 +1,126 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define PERCORSO "../input files/E3/easy_test_set.txt"
-typedef struct{
-	struct {
-		int z;
-		int s;
-		int r;
-		int t;
-	}next;
-	//0=z	1=r	2=t	3=s
-	char CPietra;
-}pietra;
+#define PERCORSO "../input files/E3/hard_test_set.txt"
+//struttura collana contenente la configurazion della collana e la dimensione della stessa
 typedef struct{
 	char* conf;
 	int dim;
-	int pnt;
 }collana;
 
-void wrap_ricerca(int* disp);
-void ricerca(int prof,collana *col,int *disp,pietra* vetPietre,collana *best,int prec);
+void wrap_ricerca(int* disp,FILE* fout);
+int ricerca(int prof,collana *col,int *disp,int** matPietre,collana *best,int prec,int target);
 void terminaz(collana *col,collana *best);
 
+//genero il file out.txt solo per confrontare la soluzione generata con i risultati attesi usando un programma esterno
 int main(void){
 	FILE* fin=fopen(PERCORSO,"r");
+	FILE* fout=fopen("./out.txt","w");;
 	int NRighe=-1;
 	int Disp[4];
 	if(NULL==fin)
 		return -1;
 	fscanf(fin,"%d ",&NRighe);
-	for(int i=0;i<2;i++){
+	fprintf(fout,"%d\n",NRighe);
+	for(int i=0;i<NRighe;i++){
 		printf("======TEST:%d=======\n",i+1);
 		fscanf(fin,"%d %d %d %d ",&Disp[0],&Disp[1],&Disp[2],&Disp[3]);
 		printf("Z:%d R:%d T:%d S:%d \n",Disp[0],Disp[1],Disp[2],Disp[3]);
-		wrap_ricerca(Disp);
+		wrap_ricerca(Disp,fout);
 		printf("===================\n");
 	}
 	fclose(fin);
+	fclose(fout);
 	return 0;
 }
-
-void wrap_ricerca(int* disp){
+//inizializza tutte le strutture necessaria per la corretta generazione della solizione
+void wrap_ricerca(int* disp,FILE* fout){
 	collana col,*sol;
-	pietra vetPietre[4];
+	int **matPietre;
 	col.dim=0;
-	col.pnt=-1;
+	int dimMax=disp[0]+disp[1]+disp[2]+disp[3];
 	//dynamic allocation of col.conf free at line: 74
-	col.conf=(char*)malloc((disp[0]+disp[1]+disp[2]+disp[3])*sizeof(char));
+	col.conf=(char*)malloc(dimMax*sizeof(char));
 	//dynamic allocation of sol free at line: 76
 	sol=(collana*)malloc(sizeof(collana));
 	//dynamic allocation of sol->conf free at line: 75
-	sol->conf=(char*)malloc((disp[0]+disp[1]+disp[2]+disp[3])*sizeof(char));
-	vetPietre[0].CPietra='z';
-		vetPietre[0].next.z=1;
-		vetPietre[0].next.s=0;
-		vetPietre[0].next.r=1;
-		vetPietre[0].next.t=0;
-	vetPietre[1].CPietra='r';
-		vetPietre[1].next.z=0;
-		vetPietre[1].next.s=1;
-		vetPietre[1].next.r=0;
-		vetPietre[1].next.t=1;
-	vetPietre[2].CPietra='t';
-		vetPietre[2].next.z=1;
-		vetPietre[2].next.s=0;
-		vetPietre[2].next.r=1;
-		vetPietre[2].next.t=0;
-	vetPietre[3].CPietra='s';
-		vetPietre[3].next.z=0;
-		vetPietre[3].next.s=1;
-		vetPietre[3].next.r=0;
-		vetPietre[3].next.t=1;
-	ricerca(0,&col,disp,vetPietre,sol,-1);
+	sol->conf=(char*)malloc(dimMax*sizeof(char));
+	sol->dim=0;
+	matPietre=(int**)malloc(4*sizeof(int*));
+	for(int i=0;i<4;i++){
+		matPietre[i]=(int*)calloc(4,sizeof(int));
+	}
+	//corrispondenza indici di disp con le pietre 
+	//0=>z	1=>r	2=>t	3=>s
+	
+	//rub
+	matPietre[0][0]=1;
+	matPietre[0][1]=1;
+	//tub
+	matPietre[1][3]=1;
+	matPietre[1][2]=1;
+	//top
+	matPietre[2][0]=1;
+	matPietre[2][1]=1;
+	//smer
+	matPietre[3][3]=1;
+	matPietre[3][2]=1;
+
+	//generazione delle regole per ogni regola 
+	for(int i=1;i<=dimMax;i++){
+		printf("==%d==\n",i);
+		if(ricerca(0,&col,disp,matPietre,sol,-1,i)==0)
+			break;
+
+	}
+	printf("SOLUZIONE MIGLIORE:\nDIM: %d\n",sol->dim);
+	fprintf(fout,"%d\n",sol->dim);
+	for(int i=0;i<sol->dim;i++){
+		printf("%c ",sol->conf[i]);
+	}
+	printf("\n");
 	free(col.conf);
 	free(sol->conf);
 	free(sol);
 	return;
 }
-
-void ricerca(int prof,collana *col,int *disp,pietra* vetPietre,collana *best,int prec){
+//la funzione ritorna 1 se ha generato la collan di dimensione target altrimenti ritorna 0
+//se la funzione ritorna 1 ritorna subito al programma chiamante, altrimenti continua la ricerca 
+//di soluzione di dimensione maggiore a quella generata
+int ricerca(int prof,collana *col,int *disp,int** matPietre,collana *best,int prec,int target){
+	//corrispondenza indici di disp con le pietre 
+	//0=>z	1=>r	2=>t	3=>s
 	int flag=0;
-	//terminazione
-	
-	//ricorsione
-	//printf("%d ",prof);
-	if(0==prof||vetPietre[prec].next.z){
-		if(disp[0]>0){
-			col->conf[col->dim++]=vetPietre[0].CPietra;
-			flag=1;
-			disp[0]--;
-			ricerca(prof+1,col,disp,vetPietre,best,0);
-			//backtrack
-			col->dim--;
-			disp[0]++;
-		}
-	}
-	if(0==prof||vetPietre[prec].next.r){
-		if(disp[1]>0){
-			col->conf[col->dim++]=vetPietre[1].CPietra;
-			flag=1;
-			disp[1]--;
-			ricerca(prof+1,col,disp,vetPietre,best,1);
-			//backtrack
-			col->dim--;
-			disp[1]++;
-		}
-	}
-	if(0==prof||vetPietre[prec].next.t){
-		if(disp[2]>0){
-			col->conf[col->dim++]=vetPietre[2].CPietra;
-			flag=1;
-			disp[2]--;
-			ricerca(prof+1,col,disp,vetPietre,best,2);
-			//backtrack
-			col->dim--;
-			disp[2]++;
-		}
-	}
-	if(0==prof||vetPietre[prec].next.s){
-		if(disp[3]>0){
-			col->conf[col->dim++]=vetPietre[3].CPietra;
-			flag=1;
-			disp[3]--;
-			ricerca(prof+1,col,disp,vetPietre,best,3);
-			//backtrack
-			col->dim--;
-			disp[3]++;
-		}
-	}
-	if(0==flag){
+	if(prof>=target){
 		terminaz(col, best);
-		//printf("prof: %d rimasti: %d %d %d %d\n",prof,disp[0],disp[1],disp[2],disp[3]);
-		return;
+		return 1;
 	}
+	for (int i=0;i<4;i++){
+		if(prof==0||matPietre[prec][i]){
+			if(prof==0||disp[i]>0){
+				flag=1;
+				col->conf[col->dim++]=(char)((char)'0'+i);
+				disp[i]--;
+				if(ricerca(prof+1,col,disp,matPietre,best,i,target)==1){
+					col->dim--;
+					disp[i]++;
+					return 1;
+				}
+				col->dim--;
+				disp[i]++;
+			}
+		}
+	}
+	return 0;
 }
+//confronta ed eventualmente sostituisce i dati della migliore soluzion fino ad ora generata 
+//con i dati della collana appena generata
 void terminaz(collana *col,collana *best){
+	//in caso di parità di dimensione si mantiene come migliore la prima generata in ordina temporale 
 	if(best->dim>=col->dim){
-		return ;
+		return;
 	}
 	best->dim=col->dim;
 	strcpy(best->conf,col->conf);
-	printf("nuove best:%d trovato:   ",col->dim);
-	for(int i=0;i<col->dim;i++){
-		printf("%c ",col->conf[i]);
-	}
-	printf("\n");
 	return;
 }
